62c3c86ba828e35ef7b69a308186e2b263eca647
chemphys <mrierari@ucsd.edu>
2018-01-23 10:52:50 -0800

    Fixed typo in monte carlo

--- integrate/stochastic/monte_carlo.py
+++ integrate/stochastic/monte_carlo.py
@@ -13,7 +13,7 @@ def monte_1d(x, f, trials):
 
 def monte_2d(f, v, domain, trials):
    area = np.prod(domain[1] - domain[0])
-   matrix = np.sqrt(area) * float(np.random.rand(trials, 2)) + np.min(domain)
+   matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
    v_eval = v(matrix) 
    idx = np.where(v_eval < 1.0)
    points_inside = v_eval[idx]
