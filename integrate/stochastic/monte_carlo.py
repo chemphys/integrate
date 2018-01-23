d7b0c1f68f8c62520e40994c4dde9b58ac8652f0
chemphys <mrierari@ucsd.edu>
2018-01-23 10:50:40 -0800

    Fixed MonteCarlo 2D bug for python 2.7

--- integrate/stochastic/monte_carlo.py
+++ integrate/stochastic/monte_carlo.py
@@ -13,9 +13,9 @@ def monte_1d(x, f, trials):
 
 def monte_2d(f, v, domain, trials):
    area = np.prod(domain[1] - domain[0])
-   matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
+   matrix = np.sqrt(area) * float(np.random.rand(trials, 2)) + np.min(domain)
    v_eval = v(matrix) 
    idx = np.where(v_eval < 1.0)
    points_inside = v_eval[idx]
    k = points_inside.size
-   return area * k / trials
+   return area * float(k) / float(trials)
